6c3ff029471d2c3b1647566647429a4e9c165741
Lord Dickenstein <jddestacamento@iskolarngbayan.pup.edu.ph>
2022-07-09 14:35:53 +0800

    Updated commands list

--- mypackage/config.py
+++ mypackage/config.py
@@ -2,9 +2,24 @@ BOT_TOKEN = 'ODE2OTM0MzA3NzgwMjMxMTc4.GgsdKt.Apb_8d89yArsIx2JmstImuYM2jQxKIAPHXW
 
 COMMANDS = [
         {'command': '!list', 'response': 'lists all commands'},
-        {'command': 'sorry', 'response': 'NEVER USE THIS COMMAND. THIS IS FORBIDDEN!!!'},
+        {'command': '!sorry', 'response': 'NEVER USE THIS COMMAND. THIS IS FORBIDDEN!!!'},
+        {'command': '!dad-jokes', 'response': 'generates a random dad joke'},
+        {'command': '!jokes', 'response': 'generates any random joke'},
+        {'command': '!jokes-any', 'response': 'generates a random joke'},
+        {'command': '!jokes-christmas', 'response': 'generates a random christmas joke'},
+        {'command': '!jokes-dark', 'response': 'generates a random dark joke'},
+        {'command': '!jokes-explicit', 'response': 'generates a random explicit joke'},
+        {'command': '!jokes-misc', 'response': 'generates a random miscellaneous joke'},
+        {'command': '!jokes-nsfw', 'response': 'generates a random nsfw joke'},
+        {'command': '!jokes-political', 'response': 'generates a random political joke'},
+        {'command': '!jokes-programming', 'response': 'generates a random programming joke'},
+        {'command': '!jokes-pun', 'response': 'generates a random pun'},
+        {'command': '!jokes-racist', 'response': 'generates a random racist joke'},
+        {'command': '!jokes-religious', 'response': 'generates a random religious joke'},
+        {'command': '!jokes-sexist', 'response': 'generates a random sexist joke'},
+        {'command': '!jokes-spooky', 'response': 'generates a random spooky joke'},
         {'command': 'more like', 'response': 'more like Bore Ragnarok!'},
-        {'command': 'literally anything? more like', 'response': 'more like Bore Ragnarok!'},
+        {'command': '<literally anything>? more like', 'response': 'more like Bore Ragnarok!'},
     ]
 
 SORRY_WORDS = ['sorry', 'sorre', 'sorreh', 'sorrey', 'sori', '!sorry']
