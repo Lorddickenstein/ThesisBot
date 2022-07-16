1b92c2c94a31efe5709c052584166c8d9286d441
Lord Dickenstein <jddestacamento@iskolarngbayan.pup.edu.ph>
2022-07-16 23:29:35 +0800

    Added new table monitored_words

--- mypackage/database.py
+++ mypackage/database.py
@@ -62,9 +62,13 @@ class Database:
 		# print(query)
 
 		print(f'   Inserting record to {table_name}...')
-		self.cur.execute(query)
-		self.conn.commit()
-		print('   Inserting record successful.')
+		try:
+			self.cur.execute(query)
+			self.conn.commit()
+			print('   Inserting record successful.')
+		except Error as err:
+			self.conn.rollback()
+			print('   Inserting unsuccessful.', err)
 
 
 	def count_mentions(self, table_name, **args):
@@ -92,8 +96,53 @@ class Database:
 			return rows
 
 
-	def get_leaderboards(self):
-		pass
+	def get_leaderboards(self, word=None, limit=5):
+		""" display the leaderboards """
+
+
+		def get_rows(word, limit):
+			""" return results from query as rows """
+			query = f'''
+					SELECT count(*) as count, author, word
+					FROM word_counter
+					WHERE word = '{word}'
+					GROUP BY author, word
+					ORDER BY word, count DESC
+					LIMIT {limit}'''
+			rows = self.cur.execute(query).fetchall()
+			return rows
+
+
+		leaderboards = {}
+		'''
+		sample data:
+			leaderboards = {
+				'sanaol': [
+					{'count': 15, 'author': '756084838154633237'},
+					{'count': 12, 'author': '756084838154633238'},
+					{'count': 1, 'author': '756084838154633239'},
+					],
+				'naol': [
+					{'count': 14, 'author': '756084838154633237'},
+					{'count': 22, 'author': '756084838154633238'},
+					{'count': 10, 'author': '756084838154633239'},
+					],
+			}
+		'''
+
+		if word:
+			rows = get_rows(word, limit)
+			leaderboards[word] = [{'count': row[0], 'author': row[1]} for row in rows]
+		else:
+			query = 'SELECT DISTINCT word FROM monitored_words'
+			rows = self.cur.execute(query).fetchall()
+			words_list = [row['word'] for row in rows]
+
+			for word in words_list:
+				rows = get_rows(word, limit)
+				leaderboards[word] = [{'count': row[0], 'author': row[1]} for row in rows]
+				
+		return leaderboards
 
 
 	def close(self):
